30a3a256393a98618e876bbb1bb5cc46c4aeaf37
zhanggongyuan <1205851537@qq.com>
2021-08-22 19:37:02 +0800

    方法

--- src/main/java/com/ctgu/j604/springbootchat/service/MessageService.java
+++ src/main/java/com/ctgu/j604/springbootchat/service/MessageService.java
@@ -4,6 +4,8 @@ import com.ctgu.j604.springbootchat.model.TUser;
 import com.ctgu.j604.springbootchat.utils.Message;
 import com.ctgu.j604.springbootchat.websocket.ChatEndPoint;
 
+import java.util.Map;
+
 public interface MessageService {
     void onlineBroadcastToFriends(ChatEndPoint chatEndPoint, Message message);
     void sendMessageOneToOne(ChatEndPoint chatEndPoint, Message message);
@@ -11,5 +13,7 @@ public interface MessageService {
     void acceptReadMessageConfirm(String toUserNum, String fromUserNum);
     void sendMessageOneToGroup(ChatEndPoint chatEndPoint, Message message);
     void sendAddFriendMessageOneToOne(ChatEndPoint chatEndPoint, Message message);
+    void sendAgreeAddFriendMessageOneToOne(ChatEndPoint chatEndPoint, Message message);
+    void sendRefuseAddFriendMessageOneToOne(ChatEndPoint chatEndPoint, Message message);
     void sendInviteMessage(TUser tUser, String[] toInviteNums,String groupNum,String groupName);
 }
